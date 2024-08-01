6d5224847c74ba4701335db77162be29db865ad2
jhsongok <66316315+jhsongok@users.noreply.github.com>
2024-08-01 13:55:22 +0900

    Add files via upload

--- application/1.object_counting/object_counting.py
+++ application/1.object_counting/object_counting.py
@@ -6,15 +6,15 @@ from ultralytics import YOLO, solutions
 model = YOLO("yolov8n.pt")
 
 # Open the video file
-cap = cv2.VideoCapture("test1.mp4")
+cap = cv2.VideoCapture("test2.mp4")
 assert cap.isOpened(), "Error reading video file"
 
 # Get video properties: width, height, and frames per second (fps)
 w, h, fps = (int(cap.get(x)) for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT, cv2.CAP_PROP_FPS))
 
 # Define points for a line or region of interest in the video frame
-region_points = [(216, 415), (1130, 415), (1130, 445), (216, 445)]   # test1.mp4 region points
-#region_points = [(200, 500), (1050, 500), (1050, 460), (200, 460)]   # test2.mp4 region points
+#region_points = [(216, 415), (1130, 415), (1130, 445), (216, 445)]   # test1.mp4 region points
+region_points = [(200, 500), (1050, 500), (1050, 460), (200, 460)]   # test2.mp4 region points
 
 # Define region points as a polygon with 5 points
 #region_points = [(20, 400), (1080, 404), (1080, 360), (20, 360), (20, 400)]
@@ -26,13 +26,13 @@ region_points = [(216, 415), (1130, 415), (1130, 445), (216, 445)]   # test1.mp4
 classes_to_count = [0, 2]  # Class IDs for person and car
 
 # Initialize the video writer to save the output video
-video_writer = cv2.VideoWriter("object_counting_output.avi", cv2.VideoWriter_fourcc(*"mp4v"), fps, (w, h))
+video_writer = cv2.VideoWriter("object_counting_output2.avi", cv2.VideoWriter_fourcc(*"mp4v"), fps, (w, h))
 
 # Initialize the Object Counter with visualization options and other parameters
 counter = solutions.ObjectCounter(
     view_img=True,                   # Display the image during processing
     reg_pts=region_points,          # Region of interest points
-    classes_names=model.names,  # Class names from the YOLO model
+    names=model.names,  # Class names from the YOLO model
     draw_tracks=True,                # Draw tracking lines for objects
     line_thickness=2,                 # Thickness of the lines drawn
     view_in_counts = True,          # Flag to control whether to display the in counts on the video stream.
